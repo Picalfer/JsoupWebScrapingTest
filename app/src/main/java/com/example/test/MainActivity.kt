dbbdc4d624d20e1c65459ed2530b95a209a1f86a
Arthur Kozhemyakin <trable228@icloud.com>
2023-06-19 22:00:19 +0300

    test josup

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.test.databinding.ActivityMainBinding
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.jsoup.Jsoup
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +15,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        GlobalScope.launch {
+            val document =
+                Jsoup.connect("https://www.youtube.com/watch?v=pGyszRRATmQ&ab_channel=NowAndroid")
+                    .get()
+            Log.d("INFO", document.title())
+        }
     }
 }
